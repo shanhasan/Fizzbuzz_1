66ef440defa951e7091464cea87f610303a0f450
Shan Hasan <shan119@hotmail.co.uk>
2014-11-05 11:33:58 +0000

    added is divisible by five

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,3 +1,7 @@
 def is_divisible_by_three?(number)
   number % 3 == 0
 end
+
+def is_divisible_by_five?(number)
+  true
+end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -7,6 +7,10 @@ describe "fizzbuzz" do
       expect(is_divisible_by_three?(3)).to be true
     end
 
+    it "will be divisible by five" do
+      expect(is_divisible_by_five?(5)).to be true
+    end
+
   end
 
   context "knows when a number is not divisible by three" do
