bd4983737d1468d54c1d49dd6a85701e5b7c17ed
Shan Hasan <shan119@hotmail.co.uk>
2014-11-05 11:22:16 +0000

    divisible by three

---
+++ spec/fizzbuzz_spec.rb
@@ -0,0 +1,13 @@
+require "fizzbuzz"
+
+describe "fizzbuzz" do
+  context "When number is divisible by" do
+
+    it "will be will be divisible by three" do  
+      expect(is_divisible_by_three?(3))
+    end
+
+
+  end
+  
+end
