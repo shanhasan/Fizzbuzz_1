40b0b79fae2ade9f8bfa39f24152511513c0c309
Shan Hasan <shan119@hotmail.co.uk>
2014-11-05 11:32:09 +0000

    not divisible by three

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -3,8 +3,16 @@ require "fizzbuzz"
 describe "fizzbuzz" do
   context "When number is divisible by" do
 
-    it "will be will be divisible by three" do  
-      expect(is_divisible_by_three?(3))
+    it "will be divisible by three" do  
+      expect(is_divisible_by_three?(3)).to be true
+    end
+
+  end
+
+  context "knows when a number is not divisible by three" do
+
+    it "will not be divisible by three" do
+      expect(is_divisible_by_three?(1)).not_to be true
     end
 
 
